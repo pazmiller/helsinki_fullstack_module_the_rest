471429662714b7d5fbecf46b02931f4d52dd3936
pazmiller <a309513511@gmail.com>
2023-07-08 11:20:45 +0800

    part 1
    Finished all the exercises in part1

--- part1/my-app/src/example.js
+++ part1/my-app/src/example.js
@@ -1,29 +1,29 @@
-import React from "react"
-import ReactDOM from "react-dom"
-/** @jsx React.DOM */
-const Hello = (props) => {
-    const { name, age } = props
-    const bornYear = () => new Date().getFullYear() - props.age
-    return (
-        <div>
-            <p>
-                Hello {name}, you are {age} years old
-            </p>
-            <p>And you were born in {bornYear()}</p>
-        </div>
-    )
+import React, { useState } from "react"
+
+
+function getRandom(min, max){
+    return Math.floor(Math.random()*(max-min+1)) + min
 }
-
 const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
+    console.log(anecdotes.length)
+
+    const [selected, setSelected] = useState(0)
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={23 + 10} />
-            <Hello name={name} age={age} />
+            {anecdotes[selected]}
         </div>
+    
     )
 }
 
