3e46f164471d39c205ab8fa16dcd311d79162a94
pazmiller <a309513511@gmail.com>
2023-09-15 16:49:45 +0800

    Part 3a
    Completed exercises for 3a

--- part3/index.js
+++ part3/index.js
@@ -1,91 +1,112 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
 
-// 使用 express.json() 中间件来解析 JSON 请求体
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
+
+morgan.token('postdata', (req) => {
+  return req.method === 'POST'? JSON.stringify(req.body): ''
+}
+)
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 app.use(express.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!<h1>')
-})
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => { 
-  response.json(notes)
-})
-
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+let data = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
+]
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for 2 people<p>
+     <p>Wed Sep 14 2023 GMT (Eastern Habibi Standard Time)<p> `)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.get('/api/persons', (request, response) => {
+  response.json(data)
+})
+
+app.get('/api/persons/:id', (request, response) =>{
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  const foundPerson = data.filter(person => person.id === id)
+  response.json(foundPerson)
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-  return maxId + 1
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const afterDeletion = data.filter(person => person.id != id)
+  response.json(afterDeletion)
+}
+)
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1))  + min
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
+  const id = randomNumber(5, 99)
   const body = request.body
-
-  if (!body.content){
-    return response.status(400).json({
-      error:'The content is empty'
-    })
+  if (!body.name || !body.number){
+    return response.status(400).json(
+      {
+        error: 'The name or number is missing'
+      }
+    )
   }
 
-  const note = {
-    content:body.content,
-    important:body.important || false,
-    id:generateId()
+  const alreadyExisting = data.find(person => person.name === body.name)
+
+  if(alreadyExisting){
+    return response.status(400).json(
+  {
+    error: 'The name already exists in the record'
+  }
+)}
+
+  const person = { 
+    id: id,
+    name: body.name, 
+    number: body.number
   }
 
-  notes = notes.concat(note)
-  console.log(notes)
-  response.json(notes)
+  data = data.concat(person)
+  response.json(data)
 })
 
-// app.post('/api/notes/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   const note = notes.find(note => note.id === id)
-//   console.log(note)
-//   response.json(note)
-// })
+
+  morgan(':method :url :status :res[content-length] - :response-time ms')
+
+
 
 const PORT = 3001
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () =>{
+  console.log(`Sever is running on PORT:${PORT}`)
 })
+
